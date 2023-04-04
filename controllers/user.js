d41a80289b20136356ff36d301c31922617d3ccd
Remees Salam <remeessalam@Remeess-Air.bbrouter>
2023-04-04 09:27:12 +0530

    error and jwt token and user controller is completed

--- controllers/user.js
+++ controllers/user.js
@@ -21,3 +21,19 @@ export const deleteUser = async(req, res, next)=>{
         next(error);
     }
 }
+export const getUser = async (req,res,next)=>{
+    try {
+      const user = await User.findById(req.params.id);
+      res.status(200).json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+  export const getUsers = async (req,res,next)=>{
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      next(err);
+    }
+  }
